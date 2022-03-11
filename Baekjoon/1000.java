553425258ac963fd532bfdb9b410c45dcd0fad54
hyeonwook98 <76507547+hyeonwook98@users.noreply.github.com>
2022-03-12 01:46:38 +0900

    A+B
    https://www.acmicpc.net/problem/1000

---
+++ Baekjoon/1000.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner input = new Scanner(System.in);
+		int x;
+		int y;
+		int sum;
+		
+		/*System.out.print("첫번째 숫자");*/
+		x = input.nextInt();
+		/*System.out.print("두번째 숫자");*/
+		y = input.nextInt();
+		
+		sum= x+y;
+		System.out.println(sum);
+	}
+
+}
