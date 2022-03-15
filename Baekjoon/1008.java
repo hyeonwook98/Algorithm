99717ed8e41d57c1329d220316bd0ab37cac5667
hyeonwook98 <76507547+hyeonwook98@users.noreply.github.com>
2022-03-15 18:06:11 +0900

    A/B
    https://www.acmicpc.net/problem/1008

---
+++ Baekjoon/1008.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner input = new Scanner(System.in);
+		double x;
+		double y;
+		double result;
+		
+		/*System.out.print("첫번째 숫자");*/
+		x = input.nextInt();
+		/*System.out.print("두번째 숫자");*/
+		y = input.nextInt();
+		
+		result= x/y;
+		System.out.println(result);
+	}
+
+}
