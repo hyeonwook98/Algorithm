2f8a1126b55705eea701f87ba3d07f9ddc054555
hyeonwook98 <76507547+hyeonwook98@users.noreply.github.com>
2022-04-29 11:31:06 +0900

    최소공배수
    https://www.acmicpc.net/problem/1934

---
+++ Baekjoon/1934.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main(void) {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int t,f,s,tmp,max,min;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		cin >> f >> s;
+		if (f > s) {
+			max = f; min = s;
+			while (1) {
+				if (f%s == 0)
+					break;
+				tmp = f%s; f = s; s = tmp;
+			}
+			cout << (max*min) / s<<endl;
+		}
+		else {
+			max = s; min = f;
+			while (1) {
+				if (s%f == 0)
+					break;
+				tmp = s%f; s = f; f = tmp;
+			}
+			cout << (max*min) / f<<endl;
+		}
+	}
+}
