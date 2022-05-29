16ff70b5a82a1bcd910ae143cbeac27c99bc75b3
hyeonwook98 <76507547+hyeonwook98@users.noreply.github.com>
2022-05-29 15:36:02 +0900

    구구단
    https://www.acmicpc.net/problem/2739

---
+++ Baekjoon/2739.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner input = new Scanner(System.in);
+		
+		int number;
+		number=input.nextInt();
+		for (int i=1;i<10;i++) {
+			System.out.println(number+" * "+i+" = "+number*i);
+		}
+	}
+
+}
