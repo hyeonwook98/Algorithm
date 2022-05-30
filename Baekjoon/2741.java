910b90d40d999c0d0251fc298081a545cf1f8e4d
hyeonwook98 <76507547+hyeonwook98@users.noreply.github.com>
2022-05-30 13:49:21 +0900

    N 찍기
    https://www.acmicpc.net/problem/2741

---
+++ Baekjoon/2741.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner input = new Scanner(System.in);
+		int number;
+		int j=1;
+		number=input.nextInt();
+		for(int i=0;i<number;i++) {
+			
+			System.out.println(j);
+			j++;
+		}
+	}
+
+}
