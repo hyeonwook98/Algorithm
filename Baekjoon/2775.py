35b9c0a87828d57ac9830523bd4994a505c9e243
hyeonwook98 <76507547+hyeonwook98@users.noreply.github.com>
2022-06-05 12:15:27 +0900

    부녀회장이 될테야
    https://www.acmicpc.net/problem/2775

---
+++ Baekjoon/2775.py
@@ -0,0 +1,9 @@
+t=int(input())
+for i in range(t):
+  k=int(input())
+  n=int(input())
+  people = [i for i in range(1,n+1)]
+  for j in range(k):
+    for k in range(1,n):
+      people[k]+=people[k-1]
+  print(people[-1])
