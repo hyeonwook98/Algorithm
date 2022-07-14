8ef9068a51022b8cfbbc664cebd3860d1ea9db8c
hyeonwook98 <76507547+hyeonwook98@users.noreply.github.com>
2022-07-14 15:49:31 +0900

    시험 성적
    https://www.acmicpc.net/problem/9498

---
+++ Baekjoon/9498.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner input = new Scanner(System.in);
+		int score;
+		
+		
+		/*System.out.print("첫번째 숫자");*/
+		score = input.nextInt();
+		
+		
+		if(90<=score)
+			System.out.println("A");
+		else if(80<=score)
+			System.out.println("B");
+		else if(70<=score)
+			System.out.println("C");
+		else if(60<=score)
+			System.out.println("D");
+		else
+			System.out.println("F");
+	}
+
+}
