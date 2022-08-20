1c3e7d577e792a9308c563610684a2ffbcd59d83
hyeonwook98 <76507547+hyeonwook98@users.noreply.github.com>
2022-08-21 00:05:31 +0900

    A+B-4
    https://www.acmicpc.net/problem/10951

---
+++ Baekjoon/10951.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner input = new Scanner(System.in);
+		
+		while(input.hasNextInt()) {
+			int a = input.nextInt();
+			int b = input.nextInt();
+			System.out.println(a+b);
+			
+			
+		}
+	}
+
+}
