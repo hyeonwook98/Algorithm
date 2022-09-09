5c83975d82b8f5d0b8abbad1983d3915804102db
hyeonwook98 <76507547+hyeonwook98@users.noreply.github.com>
2022-09-09 18:17:20 +0900

    최대 힙
    https://www.acmicpc.net/problem/11279

---
+++ Baekjoon/11279.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+int main(void) {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	priority_queue<int> q;
+	int n,x;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> x;
+		if (x == 0) {
+			if (q.empty())
+				cout << 0 << '\n';
+			else {
+				cout << q.top()<<'\n';
+				q.pop();
+			}
+		}
+		else {
+			q.push(x);
+		}
+	}
+}
