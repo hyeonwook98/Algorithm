4ac79c7933c2d106e04ad863b6a14f59ae2316f2
hyeonwook98 <76507547+hyeonwook98@users.noreply.github.com>
2022-09-16 15:57:09 +0900

    아스키 코드
    https://www.acmicpc.net/problem/11654

---
+++ Baekjoon/11654.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner input = new Scanner(System.in);
+	
+		String c = input.nextLine();
+		int result = c.charAt(0);
+		System.out.print(result);
+	}
+}
