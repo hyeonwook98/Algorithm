7e1f6c0db6eba97ac3a57937b1ec6f4590615277
hyeonwook98 <76507547+hyeonwook98@users.noreply.github.com>
2022-09-26 16:02:45 +0900

    Create 15552.java
    https://www.acmicpc.net/problem/15552

---
+++ Baekjoon/15552.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		int j =Integer.parseInt(br.readLine());
+		
+		
+		for (int i=0;i<j;i++) {
+			st = new StringTokenizer(br.readLine());
+			int a=Integer.parseInt(st.nextToken());
+			int b=Integer.parseInt(st.nextToken());
+			
+			int sum=a+b;
+			bw.write(sum+"\n");
+			
+		}
+		bw.flush();
+		
+	}
+
+}
