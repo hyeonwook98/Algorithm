81973f395a70aefbd86251e2fb1896d2ff3c46d0
hyeonwook98 <76507547+hyeonwook98@users.noreply.github.com>
2022-10-05 14:16:17 +0900

    그림
    https://www.acmicpc.net/problem/1926

--- Baekjoon/1926.py
+++ Baekjoon/1926.py
@@ -1,11 +1,10 @@
 from collections import deque
-
 import sys
 input = sys.stdin.readline
 
-n,m = map(int,input().split())
-map = [list(map(int,input().split())) for _ in range(n)]
-chk = [[False] * m for _ in range(n)]
+n, m = map(int, input().split())
+map = [list(map(int, input().split())) for _ in range(n)]
+visit = [[False] * m for _ in range(n)]
 
 cnt = 0
 maxv = 0
@@ -13,32 +12,31 @@ maxv = 0
 dx = [1,0,-1,0]
 dy = [0,1,0,-1]
 
-
-def bfs(y, x):
+def bfs(j,i):
   rs = 1
   q = deque()
-  q.append((y,x))
+  q.append((j,i))
+
   while q:
-    ey, ex = q.popleft()
+    r, c = q.popleft()
     for k in range(4):
-      nx = ex + dx[k]
-      ny = ey + dy[k]
-      if 0<=ny<n and 0<=nx<m:
-        if map[ny][nx] == 1 and chk[ny][nx] == False:
+      row = r + dy[k]
+      col = c + dx[k]
+      if 0<=row<n and 0<=col<m:
+        if map[row][col] == 1 and visit[row][col] == False:
+          visit[row][col] = True
           rs += 1
-          chk[ny][nx] = True
-          q.append((ny,nx))
+          q.append((row,col))
+
   return rs
+      
 
 for j in range(n):
   for i in range(m):
-    if map[j][i] == 1 and chk[j][i] == False:
-      # 방문 여부 갱신
-      chk[j][i] = True
-      # 전체 그림 갯수를 +1
-      cnt += 1
-      # BFS로 그림 크기를 구해주고 최대값 갱신
-      maxv = max(maxv, bfs(j,i))
+    if map[j][i] == 1 and visit[j][i] == False:
+      visit[j][i] = True
+      cnt+=1
+      maxv=max(maxv, bfs(j,i))
 
 print(cnt)
 print(maxv)
