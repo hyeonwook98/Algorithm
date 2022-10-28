a82da41d0194b00af909cff139587a4618b10c8c
hyeonwook98 <76507547+hyeonwook98@users.noreply.github.com>
2022-10-28 11:41:54 +0900

    요세푸스 문제
    https://www.acmicpc.net/problem/1158

--- Baekjoon/1158.py
+++ Baekjoon/1158.py
@@ -2,20 +2,16 @@ n,k = map(int,input().split())
 
 list = [i for i in range(1,n+1)]
 
-index = k-1
+index = 0
 print("<",end = '')
 
 for _ in range(n):
+  index += k-1
+  index %= len(list)
   print(list[index], end='')
   list.remove(list[index])
-  index-=1
   if len(list) ==0:
     break
-
   print(",", end=' ')
-  for _ in range(k):
-    index+=1
-    if(index>len(list)-1):
-      index=0
 
 print(">",end = '')
