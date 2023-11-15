17bfe79677f1cf4f3b20d87c476d67a8b176e1eb
HyunWook Kim <76507547+hyeonwook98@users.noreply.github.com>
2023-11-15 16:21:37 +0900

    올바른 괄호
    https://school.programmers.co.kr/learn/courses/30/lessons/12909?language=java

---
+++ Programmers/올바른 괄호.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < s.length(); i++){
+            if (s.charAt(i) == '('){
+                stack.push('(');
+            } else{
+                if (stack.size() != 0 && stack.peek() == '('){
+                    stack.pop();
+                } else{
+                    answer = false;
+                    break;
+                }
+            }
+        }
+        if (stack.size() != 0){
+            answer = false;
+        }
+
+        return answer;
+    }
+}
